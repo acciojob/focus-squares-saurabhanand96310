8f59eef525e21d71ec1f744c53d0aa12da2ba28f
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-09-24 16:18:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let squares=document.getElementsByClassName("square");
+squares=Array.from(squares);
+squares.forEach((square)=>{
+square.addEventListener("mouseover", (e)=>{
+	squares.forEach((s)=>{
+		s.style.backgroundColor="#6F4E37";
+	})
+	e.target.style.backgroundColor="#E6E6FA";
+})
+square.addEventListener("mouseout", (e)=>{
+	squares.forEach((s)=>{
+		s.style.backgroundColor="#E6E6FA";
+		})
+	})
+	})
+	// e.style.backgroudColor="#E6E6FA";
+// })
+
+
+
+	
